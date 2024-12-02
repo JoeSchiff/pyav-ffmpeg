ac6f9f79b7ed10761eea9f00e4f3f56aebd839e2
Joe Schiff <41972063+JoeSchiff@users.noreply.github.com>
2024-12-02 14:01:50 -0500

    Update cibuildpkg.py

--- scripts/cibuildpkg.py
+++ scripts/cibuildpkg.py
@@ -226,7 +226,7 @@ class Builder:
         if package.name == "ffmpeg":
             print(99999999)
             try:
-                shutil.copy("C:\\cibw\\vendor\\lib\\pkgconfig\\aom.pc", "/usr/share/pkgconfig/aom.pc")
+                shutil.copy("C:\\cibw\\vendor\\lib\\pkgconfig\\aom.pc", "C:\\msys64\\usr\\share\\aom.pc")
             except Exception as err:
                 print(err)
         
